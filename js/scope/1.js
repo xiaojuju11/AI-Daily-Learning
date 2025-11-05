e6772f6979bf4a8b04e13823a5f9f75ab06271e2
小小橘橘啊 <3432909760@qq.com>
2025-11-05 11:05:41 +0800

    作用域

---
+++ js/scope/1.js
@@ -0,0 +1,8 @@
+// console.log(a);
+// var a = 1
+//undefined
+
+//梳理
+var a
+console.log(a)
+a = 1
---
+++ js/scope/3.js
@@ -0,0 +1,9 @@
+// console.log(a);
+// var a = 1
+
+// let a = 1
+// a = 2
+
+const a = 3
+a = 4
+console.log(a);

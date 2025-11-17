c7cb0acee291bf907cac478325e1c708e5c5ec24
小小橘橘啊 <3432909760@qq.com>
2025-11-17 22:49:34 +0800

    对象

---
+++ js/对象/2.js
@@ -0,0 +1,19 @@
+// const obj = {
+//   a: 1
+// }
+
+// const obj = new Object()  // 构造函数
+// obj.name = '利婕'
+// const abc = 'age'
+// obj[abc] = 18
+// delete obj[abc]
+// console.log(obj);
+
+
+function Person () {
+
+}
+
+const obj = new Person()
+
+console.log(obj);
---
+++ js/对象/4.js
@@ -0,0 +1,10 @@
+function person() {
+  const _this = {}
+  _this.name = 'su7'
+  _this.color = 'green'
+  return _this
+}
+
+const obj = person()
+
+console.log(obj);
